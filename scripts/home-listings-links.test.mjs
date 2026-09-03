ae9880c534050315d0f5cd891924abbc77cc89f8
Bonobo791 <andrew.weilbacher@gmail.com>
2026-09-03 14:58:30 -0400

    fix(home): avoid dynamic regex in listings test

--- scripts/home-listings-links.test.mjs
+++ scripts/home-listings-links.test.mjs
@@ -7,6 +7,6 @@ const featuredListingsUrl = 'https://www.thelippincottteamlistings.com/featured-
 const legacyListingsUrl = 'https://www.thelippincottteamlistings.com/property-search/results/';
 
 test('all home-page live-listings links use the team featured listings page', () => {
-	assert.equal(homePage.match(new RegExp(featuredListingsUrl, 'g'))?.length, 3);
-	assert.equal(homePage.match(new RegExp(legacyListingsUrl, 'g'))?.length, 1);
+	assert.equal(homePage.split(featuredListingsUrl).length - 1, 3);
+	assert.equal(homePage.split(legacyListingsUrl).length - 1, 1);
 });
